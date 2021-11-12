126af802e50372c7d0a3d369bea258c3d70d4794
Nedelcho Dinev <ned.dinev@gmail.com>
2021-11-12 15:59:57 +0200

    Add files via upload

---
+++ Rectangle-from-stars.js
@@ -0,0 +1,10 @@
+function rectangleFromStars(n) {
+    
+    for (let i = 0; i < n; i++) {
+console.log("*".repeat(n));
+        }
+ }
+    
+rectangleFromStars(2);
+rectangleFromStars(3);
+rectangleFromStars(4);
